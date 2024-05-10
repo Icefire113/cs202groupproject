92e613e95a72ecb31cf012bb9bdd2fc5f1f077ff
Icefire113 <phelan.gormley@gmail.com>
2024-05-10 01:07:55 -0700

    print java + javac versions to a file when building with build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# print java versions to a file
+(javac --version && java --version) > javaVer.txt
 # clean previous build 
 rm -rf ./bin/*
 # compile main file and put out files in bin, then run it

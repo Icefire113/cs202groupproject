00e9d1672bb4afa63a5004522b45686481fb8d8d
Icefire113 <phelan.gormley@gmail.com>
2024-05-14 16:17:22 -0700

    add some stuff

--- src/BoardGraphics.java
+++ src/BoardGraphics.java
@@ -3,8 +3,8 @@ import util.*;
 public class BoardGraphics {
 
     // board sizes
-    final int boardX = 10;
-    final int boardY = 10;
+    final static int boardX = 20;
+    final static int boardY = 20;
 
     // should we use arraylists or just nested arrays for the board?
 
@@ -12,5 +12,12 @@ public class BoardGraphics {
     // putting the first apple, etc
     public static void initBoard() {
         Logging.debug("Starting board init");
+        Logging.debug("Board size (" + boardX + ", " + boardY + ")");
+    }
+
+    // called each 'frame' to update the display
+    // we should prolly move the snake around every 3/4 of a second, but should
+    // adjust it after playtesting
+    public static void update() {
     }
 }

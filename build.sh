c30af8021651efdc86615872f5bdb2550b4a1f1e
Icefire113 <phelan.gormley@gmail.com>
2024-05-29 23:24:48 -0700

    make it so javaver file has $USER appended to filename

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # print java versions to a file
-(javac --version && java --version) > javaVer.txt
+(javac --version && java --version) > javaVer.$USER.txt
 # clean previous build 
 rm -rf ./bin/*
 # compile main file and put out files in bin, then run it
